4e92bdd0bf18abbd0b1a3e741d4076ec6e9993f0
GrigoryDev013 <156224807+GrigoryDev013@users.noreply.github.com>
2024-01-12 18:45:12 +0300

    Initial commit

---
+++ Assets/Script Folder/Test.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test: MonoBehaviour
+{
+    public int Number;
+    
+    public float Drob;
+    public bool FalseTrue;
+    public string Name;
+    public int Chislo;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+
+    }
+}
